b85ccf0b38dc99bfde2e71a55d6bfd5401617435
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2025-06-05 17:43:18 -0300

    feat: Criando Formulário

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -4,8 +4,8 @@ import { Header } from "./Header"
 export function AppLayout() {
   return (
     <div className="w-screen h-screen bg-gray-400 flex flex-col items-center text-gray-100">
-      <Header />
       <main className="p-3 w-full md:w-auto">
+        <Header />
         <Outlet />
       </main>
     </div>
