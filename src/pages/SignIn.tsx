718d3d1486d3ca3b5eaa4fa3c620cfa010d7e705
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2025-06-08 19:59:23 -0300

    feat: Action

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,44 +3,40 @@ import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 
 export function SignIn() {
-    const [ email, setEmail ] = useState("")
-    const [ password, setPassword ] = useState("")
-    const [ isLoading, setIsLoading ] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
-    function onSubmit(e: React.FormEvent) {
-        e.preventDefault()
+  function onAction(formData: FormData) {
+    console.log(formData.get("email"))
+  }
 
-        console.log(email, password)
-    }
+  return (
+    <form action={onAction} className="w-full flex flex-col gap-4">
+      <Input
+        name="email"
+        required
+        legend="E-mail"
+        type="email"
+        placeholder="seu@email.com"
+      />
 
-    return (
-        <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
-            <Input 
-                required 
-                legend="E-mail" 
-                type="email" 
-                placeholder="seu@email.com"
-                onChange={(e) => setEmail(e.target.value)}
-            />
+      <Input
+        name="password"
+        required
+        legend="Senha"
+        type="password"
+        placeholder="x*Wk/;a"
+      />
 
-            <Input 
-                required 
-                legend="Senha" 
-                type="password" 
-                placeholder="x*Wk/;a"
-                onChange={(e) => setPassword(e.target.value)}
-            />
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
 
-            <Button type="submit" isLoading={isLoading}>
-                Entrar
-            </Button>
-
-            <a 
-                href="/signup"
-                className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
-            >
-                Criar conta
-            </a>
-        </form>
-    )
+      <a
+        href="/signup"
+        className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
+      >
+        Criar conta
+      </a>
+    </form>
+  )
 }
