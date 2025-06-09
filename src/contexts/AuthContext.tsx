c126980251a56e641e78b948caeed75c973fced5
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2025-06-09 20:45:18 -0300

    feat: Local Storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -6,16 +6,23 @@ type AuthContext = {
   save: (data: UserAPIResponse) => void
 }
 
+const LOCAL_STORAGE_KEY = "@refund"
+
 export const AuthContext = createContext({} as AuthContext)
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [session, setSession] = useState<null | UserAPIResponse>(null)
 
   function save(data: UserAPIResponse) {
+    localStorage.setItem(`${LOCAL_STORAGE_KEY}:user`, JSON.stringify(data.user))
+    localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token)
+    
     setSession(data)
   }
 
   return (
-    <AuthContext.Provider value={{ session, save }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ session, save }}>
+      {children}
+    </AuthContext.Provider>
   )
 }
