70da2e748f89c9ec41aa0f0c7c32de5557729750
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2025-06-11 16:56:11 -0300

    feat: Upload do arquivo

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -27,7 +27,7 @@ export function Refund() {
   const [amount, setAmount] = useState("")
   const [category, setCategory] = useState("")
   const [isLoading, setIsLoading] = useState(false)
-  const [filename, setFilename] = useState<File | null>(null)
+  const [file, setFile] = useState<File | null>(null)
 
   const navigate = useNavigate()
   const params = useParams<{ id: string }>()
@@ -42,15 +42,24 @@ export function Refund() {
     try {
       setIsLoading(true)
 
+      if (!file) {
+        return alert("Selecione um arquivo de comprovante")
+      }
+
+      const fileUploadForm = new FormData()
+      fileUploadForm.append("file", file)
+
+      const response = await api.post("/uploads", fileUploadForm)
+
       const data = refundSchema.parse({
         name,
         category,
-        amount: amount.replace(",", ".")
+        amount: amount.replace(",", "."),
       })
 
       await api.post("/refunds", {
         ...data,
-        filename: "1234567891011121314151617181920.png",
+        filename: response.data.filename,
       })
 
       navigate("/confirm", { state: { fromSubmit: true } })
@@ -132,8 +141,8 @@ export function Refund() {
         </a>
       ) : (
         <Upload
-          filename={filename && filename.name}
-          onChange={(e) => e.target.files && setFilename(e.target.files[0])}
+          filename={file && file.name}
+          onChange={(e) => e.target.files && setFile(e.target.files[0])}
         />
       )}
 
